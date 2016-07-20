05764aa853f6c62e08b7cbf0917a6f5f74cf7a55
[01;31m[K"name"[m[K: "Brad Harris <[01;31m[K"email"[m[K: null>
2016-07-20 11:24:59 -0500

    moved jquery from cohorts to teacher_profile

---
+++ app/assets/javascripts/teacher_profile.js
@@ -0,0 +1,7 @@
+// brad's code for teacher profile
+$(document).ready(function(){
+  $(this).on("click", "#show_form_button", function(e) {
+    e.preventDefault();
+    $("#add_cohort_form").css("display", "block");
+  })
+});
