1ef4efea21cf234d81659b31fb0fcdaf72e85d1b
[01;31m[K"name"[m[K: "Brad Harris <[01;31m[K"email"[m[K: null>
2016-07-20 14:07:51 -0500

    works with known bug, if resize after loads, cohorts still takes original placement

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -23,7 +23,12 @@ $(document).ready(function() {
 			var logged_in = response.logged_in;
 			if ( logged_in === true ) {
 				$("#welcome .right").css("display", "none");
-				$("#welcome .left").animate ( {left: "37%"}, 500, function() {} );
+
+				// animate only takes numbers, so hard coding half of #welcome container, then minus half of the actual width of .left
+				var welcomeWidth = $("#welcome").width();
+				var cohortsWidth = $("#welcome .left").width();
+				var align = ((welcomeWidth/2) - (cohortsWidth/2));
+				$("#welcome .left").animate ( {left: align }, 500, function() {} );
 			}
 		});
 		
